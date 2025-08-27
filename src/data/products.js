2b8bebd74ef30e4994c9ae7fdbbde1826cdd0269
Mayron Castillo <12mayron12@gmail.com>
2025-08-27 16:36:35 -0600

    Carpetas, imagenes y info de los productos del ecommerce

---
+++ src/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    id: 1,
+    nombre: "Camiseta de Arsenal",
+    precio: 39.99,
+    imagen: "/img/arsenal.webp",
+  },
+  {
+    id: 2,
+    nombre: "Camiseta de Atletico Madrid",
+    precio: 39.99,
+    imagen: "/img/atletico.jpg",
+  },
+  {
+    id: 3,
+    nombre: "Camiseta de Barcelona",
+    precio: 39.99,
+    imagen: "/img/barca.jpg",
+  },
+  {
+    id: 4,
+    nombre: "Camiseta de Manchester City",
+    precio: 39.99,
+    imagen: "/img/city.jpg",
+  },
+  {
+    id: 5,
+    nombre: "Camiseta de Real Madrid",
+    precio: 39.99,
+    imagen: "/img/madrid.jpg",
+  },
+  {
+    id: 6,
+    nombre: "Camiseta de Liverpool",
+    precio: 39.99,
+    imagen: "/img/liverpool.jpg",
+  },
+  {
+    id: 7,
+    nombre: "Camiseta de Inter",
+    precio: 39.99,
+    imagen: "/img/inter.jpg",
+  },
+  {
+    id: 8,
+    nombre: "Camiseta de Milan",
+    precio: 39.99,
+    imagen: "/img/milan.webp",
+  },
+  {
+    id: 9,
+    nombre: "Camiseta de PSG",
+    precio: 39.99,
+    imagen: "/img/psg.jpg",
+  },
+  {
+    id: 10,
+    nombre: "Camiseta de Bayern Munich",
+    precio: 39.99,
+    imagen: "/img/bayern.jpg",
+  },
+  {
+    id: 11,
+    nombre: "Camiseta de Borussia Dortmund",
+    precio: 39.99,
+    imagen: "/img/dortmund.webp",
+  },
+  {
+    id: 12,
+    nombre: "Camiseta de Bayer Leverkusen",
+    precio: 39.99,
+    imagen: "/img/leverkusen.jpg",
+  },
+];
+
+export default products;
